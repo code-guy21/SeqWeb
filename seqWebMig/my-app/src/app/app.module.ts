cfed680630532a9a7968fe1cff764f633bcfcb67
Miguel Chavez <mac3@knights.ucf.edu>
2017-10-07 19:51:13 -0400

    start
    this is the start

---
+++ seqWebMig/my-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SoundsComponent } from './components/sounds/sounds.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'apps', component:AppComponent},
+  {path:'sound', component:SoundsComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SoundsComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
