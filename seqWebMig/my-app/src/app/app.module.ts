921c7eb297840b6a7d4de15827ce23ea27afe3c9
Miguel Chavez <mac3@knights.ucf.edu>
2017-10-07 22:47:42 -0400

    Sounds
    ability to add and delete a sound from the sounds array.

--- seqWebMig/my-app/src/app/app.module.ts
+++ seqWebMig/my-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { SoundsComponent } from './components/sounds/sounds.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TestComponent } from './components/test/test.component';
 
 const appRoutes: Routes = [
   {path:'', component:HomeComponent},
   {path:'apps', component:AppComponent},
-  {path:'sound', component:SoundsComponent}
+  {path:'sound', component:SoundsComponent},
+  {path:'test', component:TestComponent}
 ];
 
 @NgModule({
@@ -18,7 +20,8 @@ const appRoutes: Routes = [
     AppComponent,
     SoundsComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
